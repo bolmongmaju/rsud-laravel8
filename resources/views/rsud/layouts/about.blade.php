2fc28147f3fe2a0e253cf2d6a01f5e48bf90cc22
troydimarzio <troymokoagow86@gmail.com>
2023-06-08 14:27:00 +0800

    update front  foto pimpinan

--- resources/views/rsud/layouts/about.blade.php
+++ resources/views/rsud/layouts/about.blade.php
@@ -1,22 +1,19 @@
 <div class="container">
-    <div class="row align-items-center">
-        <div class="col-lg-6">
-            <div class="about-img">
-                <img src="{{ Storage::url($profil->foto_pimpinan ?? null) }}" alt="foto_pimpinan" class="img-fluid">
-                {{-- <img src="{{ asset('front/images/about/img-2.jpg') }}" alt="" class="img-fluid mt-4"> --}}
-            </div>
-        </div>
-        {{-- <div class="col-lg-4 col-sm-6">
-            <div class="about-img mt-4 mt-lg-0">
-                <img src="{{ asset('front/images/about/img-3.jpg') }}" alt="" class="img-fluid">
-            </div>
-        </div> --}}
-        <div class="col-lg-6">
+    <div class="row justify-content-center">
+        <div class="col-auto col-lg-4 shuffle-item">
+        	<div class="position-relative doctor-inner-box">
+		        <div class="doctor-profile">
+		        	<div class="doctor-img about-img">
+		               <img src="{{ Storage::url($profil->foto_pimpinan ?? null) }}" alt="foto_pimpinan" class="img-fluid img-thumbnail w-100">
+		            </div>
+	            </div>
+	      	</div>
+      </div>
+
+        <div class="col-auto col-lg-4">
             <div class="about-content pl-4 mt-lg-0">
                 <h2 class="title-color">Kata Sambutan</h2>
                 <p class="mt-4 mb-5">{!! nl2br(($profil->kata_sambutan ?? null)) !!}</p>
-
-                {{-- <a href="service.html" class="btn btn-main-2 btn-round-full btn-icon">Services<i class="icofont-simple-right ml-3"></i></a> --}}
             </div>
         </div>
     </div>
